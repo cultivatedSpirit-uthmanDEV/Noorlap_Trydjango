f260bc485ff36d0f6401497c9c14391802a6d551
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-04-05 22:41:01 +0100

    display article data from database in views

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'article',
+    
 ]
 
 ROOT_URLCONF = 'trydjango.urls'
--- trydjango/views.py
+++ trydjango/views.py
@@ -6,21 +6,33 @@ To render html web pages
 import random
 from django.http import HttpResponse
 
+
+from articles.models import Articles
+
+
+
 def home_view(request):
   """
    Take in a request (Django sends request)
    Return HTML as a response (We pick to return the response)
   """
-  number = random.randint(10,200000) # pseudocode
+  random_number = random.randint(1,4) # pseudocode
   name = 'Uthman' # hardcode
+
+   ## from the database
+
+  article_obj = Articles.objects.get(id = random_number)
+  article_content = article_obj.content
+  article_title = article_obj.title
+
 
 
    #Django Template
   H1_string = f"""
-  <h1>Hello {name} - {number}</h1>"""
+  <h1>Hello {article_title} - {article_content} ( id:{ article_obj.id})!</h1>"""
 
   p_string = f"""
-  <p>Hello {name} - {number}</p>"""
+  <p>Hello {name} - {random_number}</p>"""
 
   HTML_STRING = H1_string + p_string
   
