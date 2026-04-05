ee4b783e60f0386b2304d9ae0fa5e4de98d8eab6
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-04-05 21:43:25 +0100

    Implement create, save, and retrieve data using django ORM

--- articles/models.py
+++ articles/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class Articles(models.Model):
   title = models.TextField()
   content = models.TextField() 
+
+  ## Django shell
+
+##  obj2.save()
+#>>> obj2.id 5
+    #  >>> obj2
+     # <Articles: Articles object (5)>
+      #>>> a= Articles.objects.get(id=5)
+      #>>> a.title
+      #'this is cool'
+      #>>> b = Articles.objects.get(id=4)
+      #>>> b.title
+      #'This is my other title'
