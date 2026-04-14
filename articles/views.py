42a56d75dbdd44283ecec18b5e33f72d1671dad4
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-04-14 20:41:07 +0100

    query continues

--- articles/views.py
+++ articles/views.py
@@ -4,7 +4,16 @@ from .models import Articles
 # Create your views here.
 
 def articles_search_view(request):
-    context = {}
+    #print(dir(request))
+    print(request.GET)
+    query_dict = request.GET # this is a dictionary
+    query = query_dict.GET('q') #  <input type="text" name="q"/>
+    article_obj = None
+    if article_obj is not None:
+      article_obj = Articles.objects.get(id=query)
+    context = {
+        "object": article_obj
+        }
     return render(request, "articles/search.html",context=context)
 
 def articles_detail_view(request, id=None):
