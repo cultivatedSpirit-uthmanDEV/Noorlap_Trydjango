7074b2a456eeace32ef23bc37c336f0cfb9e52fe
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-04-30 15:07:08 +0100

    ModelForm(AuthenticationForm )
    Django in-built login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm , UserCreationForm 
 
 # Create your views here.
 
@@ -13,22 +13,26 @@ def register_view(request):
    context = {"form" : form}
    return render(request , "accounts/register.html", context)
 
+
+
 def login_view(request):
-    #if request.user.is_authenticated:
-       #return render(request,"accounts/already-logged-in.html", {})
-    if request.method == "POST":
-      username = request.POST.get("username")
-      password = request.POST.get("password")
+   if request.method == "POST":
+      form = AuthenticationForm(request, data=request.POST)
+      if form.is_valid():
+         user = form.get_user()
+         login(request, user)
+         return redirect("/")
 
-      user = authenticate(request, 
-      username=username, password=password)
-      if user is None:
-        context = {'error' : "Invalid username or password"}
-        return render(request, "accounts/login.html", context)
-      login(request, user)
-      return redirect('/admin')
+   else:
+      form = AuthenticationForm(request)
 
-    return render(request, "accounts/login.html", {})
+   context = {
+      "form" : form
+   }
+
+   return render(request, "accounts/login.html", context)
+
+
 
 def logout_view(request):
   if request.method == "POST":
