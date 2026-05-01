4705729760fafeb4e0cd2fbcca9864c7796a667e
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-05-01 23:25:16 +0100

    Clean repo and apply gitignore properly

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mv=zc&(9*=9t4i5b0x28n9&et%n(!8wg_04w3g4j=@!y_*khae'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-mv=zc&(9*=9t4i5b0x28n9&et%n(!8wg_04w3g4j=@!y_*khae')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
 
 ALLOWED_HOSTS = []
-
+if not DEBUG:
+  ALLOWED_HOSTS += [os.environ]
 
 
 # Application definition
