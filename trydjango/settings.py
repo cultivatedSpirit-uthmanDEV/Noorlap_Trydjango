2f8b8055677a0c64dc11019b388f8184f88b66c5
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-05-02 17:05:36 +0100

    update setting to remove secret key

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-mv=zc&(9*=9t4i5b0x28n9&et%n(!8wg_04w3g4j=@!y_*khae')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
