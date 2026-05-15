f4c6eab768b2b0323abd53d4cc7de5a1dece7cea
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-05-15 22:27:26 +0100

    SlugField and Slugifying

--- articles/models.py
+++ articles/models.py
@@ -1,13 +1,32 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 class Articles(models.Model):
-  title = models.CharField(max_length=120)
-  content = models.TextField() 
-  timestamp = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True )
-  publish = models.DateField(auto_now_add=False, auto_now=False, null=True)
+      title = models.CharField(max_length=120)
+      slug = models.SlugField(blank=True, null=True)
+      content = models.TextField() 
+      timestamp = models.DateTimeField(auto_now_add=True)
+      updated = models.DateTimeField(auto_now=True )
+      publish = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
+
+
+
+      def save(self, *args, **kwargs):
+            if self.slug is None:
+              self.slug = slugify(self.title)
+              super().save(*args, **kwargs)
+
+
+
+
+
+
+
+
+
+
 
   ## Django shell
 
